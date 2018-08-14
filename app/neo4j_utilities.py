0e62d1f11ed3fa2a78c38a66b9f413725feadd7a
Frank Grieshaber <frank.grieshaber@zaw.uni-heidelberg.de>
2018-08-14 07:13:43 +0200

    add browse functionality

--- app/neo4j_utilities.py
+++ app/neo4j_utilities.py
@@ -2,6 +2,40 @@ from app import app
 from neo4j.v1 import GraphDatabase, basic_auth
 
 
+def get_browse_data():
+    """ returns GODOT URIs and string of paths as a list of dictionaries
+        for browse data page on website as a list of dictionaries
+    """
+    query = "match (t:Timeline),(g:GODOT),p = shortestPath((t)-[*..15]->(g)) return p"
+    uri = "bolt://localhost:7687"
+    driver = GraphDatabase.driver(uri, auth=basic_auth(
+        app.config['NEO4J_USER'], app.config['NEO4J_PASSWORD']))
+    session = driver.session()
+    results = session.run(query)
+    browse_array = []
+    for path in results:
+        entry_dict = {}
+        path_str = ""
+        nodes = path["p"].nodes
+        for n in nodes:
+            if list(n.labels)[0] != 'Timeline':
+                label = list(n.labels)[0]
+                for k, v in n.items():
+                    if label == "GODOT" and k == 'uri':
+                        # get GODOT ID only
+                        godot = v.split("/")
+                        entry_dict['godot_uri'] = godot[-1]
+                    if k == 'type' and label != 'GODOT':
+                        if v != 'number' and v != 'reign' and v != 'mont' and v != 'day':
+                            path_str += "%s " % v
+                    if k == 'value':
+                        path_str += " %s " % v
+        entry_dict['path_str'] = path_str
+        browse_array.append(entry_dict)
+    session.close()
+    return browse_array
+
+
 def get_godot_path(godot_uri):
     """ returns all paths between Timeline node and the GODOT node
         specified by GODOT URI as list of dictionaries
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm, RomanConsularDating, CyrenaicaYears
 from app.convert import Convert_roman_calendar
-from app.neo4j_utilities import get_godot_path, get_attestations
+from app.neo4j_utilities import get_godot_path, get_attestations, get_browse_data
 
 
 @app.route('/')
@@ -18,8 +18,8 @@ def user_greeting(name):
 
 @app.route('/browse/')
 def browse():
-    # browse_data = get_browse_data()
-    return render_template('browse.html', title='Browse Data', browse_text='Browse Data')
+    browse_data = get_browse_data()
+    return render_template('browse.html', title='Browse Data', browse_data=browse_data)
 
 
 @app.route('/contact/')

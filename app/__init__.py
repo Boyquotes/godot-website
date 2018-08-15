4329383732d865c0a689559658eb81c44dbded29
Frank Grieshaber <frank.grieshaber@zaw.uni-heidelberg.de>
2018-08-15 18:02:00 +0200

    reorganized imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
-from flask import Flask
-from flask import render_template
-from flask_bootstrap import Bootstrap
-from flask_simplelogin import SimpleLogin, get_username, login_required
-from app import users
 import os
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_simplelogin import SimpleLogin
+from app import users
+from app import routes
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
@@ -13,4 +13,4 @@ app.config['NEO4J_PASSWORD'] = os.environ['NEO4J_PASSWORD']
 
 SimpleLogin(app, login_checker=users.is_valid_user)
 bootstrap = Bootstrap(app)
-from app import routes
+

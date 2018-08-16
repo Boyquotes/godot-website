1c288c763bdd29f9757ac293823e340766779df0
Frank Grieshaber <frank.grieshaber@zaw.uni-heidelberg.de>
2018-08-16 14:54:59 +0200

    refactoring neo4j utils

--- app/neo4j_utilities.py
+++ app/neo4j_utilities.py
@@ -3,15 +3,13 @@ from neo4j.v1 import GraphDatabase, basic_auth
 
 
 def get_browse_data():
-    """ returns GODOT URIs and string of paths as a list of dictionaries
-        for browse data page on website as a list of dictionaries
+    """
+    returns GODOT URIs and string of paths as a list of dictionaries
+        for browse data page on website
+    :return: list of dictionaries
     """
     query = "match (t:Timeline),(g:GODOT),p = shortestPath((t)-[*..15]->(g)) return p"
-    uri = "bolt://localhost:7687"
-    driver = GraphDatabase.driver(uri, auth=basic_auth(
-        app.config['NEO4J_USER'], app.config['NEO4J_PASSWORD']))
-    session = driver.session()
-    results = session.run(query)
+    results = query_neo4j_db(query)
     browse_array = []
     for path in results:
         entry_dict = {}
@@ -23,8 +21,7 @@ def get_browse_data():
                 for k, v in n.items():
                     if label == "GODOT" and k == 'uri':
                         # get GODOT ID only
-                        godot = v.split("/")
-                        entry_dict['godot_uri'] = godot[-1]
+                        entry_dict['godot_uri'] = v.split("/")[-1]
                     if k == 'type' and label != 'GODOT':
                         if v != 'number' and v != 'reign' and v != 'mont' and v != 'day' and v != 'consulship':
                             path_str += "%s " % v
@@ -32,51 +29,53 @@ def get_browse_data():
                         path_str += " %s " % v
         entry_dict['path_str'] = path_str
         browse_array.append(entry_dict)
-    session.close()
     return browse_array
 
 
 def get_godot_path(godot_uri):
-    """ returns all paths between Timeline node and the GODOT node
-        specified by GODOT URI as list of dictionaries
+    """
+    returns all paths between Timeline node and the GODOT node
+    :param godot_uri: string/uri of GODOT ID
+    :return: list of dictionaries
     """
     query = "match (t:Timeline),(g:GODOT {uri:'%s'}),p = ((t)-[*..15]->(g)) return p" % godot_uri
-    uri = "bolt://localhost:7687"
-    driver = GraphDatabase.driver(uri, auth=basic_auth(
-        app.config['NEO4J_USER'], app.config['NEO4J_PASSWORD']))
-    session = driver.session()
-    print("get_godot_path: " + query)
-    results = session.run(query)
+    results = query_neo4j_db(query)
     paths = []
     for record in results:
         nodes = record["p"].nodes
         for n in nodes:
             if list(n.labels)[0] != 'Timeline' and list(n.labels)[0] != 'GODOT':
-                n_dict = {}
-                n_dict['label'] = list(n.labels)[0]
+                n_dict = {'label': list(n.labels)[0]}
                 for k, v in n.items():
                     n_dict[k] = str(v)
                 paths.append(n_dict)
-    session.close()
     return paths
 
 
 def get_attestations(godot_uri):
-    """ returns all attestations for specified GODOT node
-        as a list of dictionaries
+    """
+    returns all attestations for specified GODOT node
+    :param godot_uri: string/uri of GODOT ID
+    :return: list of dictionaries
     """
     query = "match (g:GODOT {uri:'%s'})--(a:Attestation) return a" % godot_uri
+    results = query_neo4j_db(query)
+    att = []
+    for record in results:
+        att.append({k: v for (k, v) in record["a"].items()})
+    return att
+
+
+def query_neo4j_db(query):
+    """
+    performs query into graph database and returns results
+    :param query: string of cypher query
+    :return: result dictionary
+    """
     uri = "bolt://localhost:7687"
     driver = GraphDatabase.driver(uri, auth=basic_auth(
         app.config['NEO4J_USER'], app.config['NEO4J_PASSWORD']))
     session = driver.session()
-    print("get_attestations: " + query)
-    results = session.run(query)
-    att = []
-    for record in results:
-        r_dict = {}
-        for k, v in record["a"].items():
-            r_dict[k] = v
-        att.append(r_dict)
+    res = session.run(query)
     session.close()
-    return att
+    return res
